4d1a60b914771614399614ae58efafb3e496f2be
greenbreakfast <lazardemin@gmail.com>
2018-11-20 22:41:58 +0000

    changed minimal build script to keep kernel intact - need this to compile working firmware

--- scripts/onion-minimal-build.sh
+++ scripts/onion-minimal-build.sh
@@ -3,7 +3,7 @@
 ## This script will change the .config file to only build packages that are included in the firmware
 ## Results in a much faster compile time
 
-sed -e "s/\(.*\)=m/\# \1 is not set/" -i .config
+sed -i '/kmod/! s/\(.*\)=m/\# \1 is not set/' .config
 
 echo "> Done"
 
